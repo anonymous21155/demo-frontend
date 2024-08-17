780b8f9b2f3173b2eacebdd2243e5e3ed62123ba
anonymous21155 <gokul.dev@10xds.com>
2024-08-17 14:32:49 +0530

    test3

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Home'
 import Hlo from './Hlo'
-import {Route,RouterProvider,Routes,createBrowserRouter,createRoutesFromElements, redirect} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
+  /*const router = createBrowserRouter(createRoutesFromElements(
     <Route>
       <Route path='/' element={<Home/>} />
       <Route path='/hello' element={<Hlo/>} />
     </Route>
-  ))
+  ))*/
   /*const [count, setCount] = useState('')
   async function handleClick () {
     await fetch('https://testbackend2123-c5ddevbvckemdvgd.eastus-01.azurewebsites.net/data', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({hello: 'hi'})}).then((res) => {res.json()}).then((data) => setCount(data.recieved), console.log(count))
   }
   */
   return (
-    <RouterProvider  router={router}/>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/hello' element={<Hlo/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
